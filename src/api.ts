b3fc00e5e11de11cbee37238e6ad44340c77b2bd
gahyeon <pgh2031@inpix.com>
2023-09-14 18:54:01 +0900

    #9.6 Home Screen part 2

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,25 @@
 const API_KEY = "c33cd4416575a2635f2ceaa5dec83b09";
 const BASE_PATH = "https://api.themoviedb.org/3/movie";
 
+interface IMovie {
+  id: number;
+  title: string;
+  overview: string;
+  backdrop_path: string;
+  poster_path: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  results: Array<IMovie>;
+  page: number;
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/now_playing?api_key=${API_KEY}`).then((response) =>
     response.json()
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,62 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { getMovies, IGetMoviesResult } from "../api";
+import styled from "styled-components";
+import { makeImagePath } from "../utils";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh;
+`;
+
+const Banner = styled.div<{ $bgPhoto: string }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)),
+    url(${(props) => props.$bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 20px;
+  font-size: 58px;
+`;
+
+const Overview = styled.p`
+  width: 50%;
+  font-size: 26px;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
   console.log(data, isLoading);
 
   return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>Home</div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner
+            $bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
   );
 }
 
